2dd612fe436ba44521a745af8c9c218c744a1b37
Anika Chowdhury <raisaanika@Valo-lagey-na.local>
2023-10-22 00:27:00 -0400

    prompt change

--- healthify_backend/healthify_backend_app/openai_integration.py
+++ healthify_backend/healthify_backend_app/openai_integration.py
@@ -10,7 +10,7 @@ def openai_request():
     }
     data = {
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": "Assume you are a health concious individual who suggests healthy recipes along with its calorie count of each ingredient from the given ingredients"}],
+        "messages": [{"role": "user", "content": "I am a healthy food enthusiast but I do not know how to make healthy food choice while cooking. I want to make a pizza. The ingredients of pizza are Flour, Salt, Sugar, Butter, Mozzarella Cheese and pepperoni. This pizza is 2000 kilocalories. I want to replace the usual ingredients for the pizza mentioned above by healthier ingredients which are less in calories so my pizza would be under 1000 kilocalories. So I choose to use Cauliflower crust instead of plain flour crust, I use less salt, I use cottage cheese instead of mozzarella cheese and chicken instead of pepperoni.\n input: Dish: Hamburger\n Calori goal: 400\n Diet: Keto"}],
         "temperature": 0.7
     }
     response = requests.post(api_url, json=data, headers=headers)
